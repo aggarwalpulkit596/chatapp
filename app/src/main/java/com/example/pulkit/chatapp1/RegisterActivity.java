48b410ea5b3deeb30013445078f0459f5a18cc25
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2017-12-24 11:26:53 +0530

    First

---
+++ app/src/main/java/com/example/pulkit/chatapp1/RegisterActivity.java
@@ -0,0 +1,120 @@
+package com.example.pulkit.chatapp1;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mEmail, mPassword, mDisplayName;
+    private Button mCreateBtn;
+    private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+
+    private ProgressDialog mRegProcess;
+
+    private DocumentReference mDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mCreateBtn = findViewById(R.id.reg_create_btn);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
+        mDisplayName = findViewById(R.id.reg_display_name);
+        mAuth = FirebaseAuth.getInstance();
+
+        mRegProcess = new ProgressDialog(this);
+
+        mToolbar = findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String display_name = mDisplayName.getEditText().toString();
+                String email = mEmail.getEditText().toString();
+                String password = mPassword.getEditText().toString();
+
+                if (!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mRegProcess.setTitle("Registering User");
+                    mRegProcess.setMessage("PLease Wait...");
+                    mRegProcess.setCanceledOnTouchOutside(false);
+                    mRegProcess.show();
+
+                    register_user(display_name, email, password);
+                }
+
+            }
+        });
+    }
+
+    private void register_user(final String display_name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+
+                            String uid = current_user.getUid();
+
+                            mDatabase = FirebaseFirestore.getInstance().document("users/" + uid);
+
+                            Map<String, String> userMap = new HashMap<>();
+                            userMap.put("name", display_name);
+                            userMap.put("status", "Hey there,I am using Chatapp");
+                            userMap.put("image", "default");
+                            userMap.put("thumb_image", "default");
+
+                            mDatabase.set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        mRegProcess.dismiss();
+                                        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(mainIntent);
+                                        finish();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            mRegProcess.hide();
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+}
