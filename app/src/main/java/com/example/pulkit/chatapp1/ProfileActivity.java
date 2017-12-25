7360da6db8cc6f828ab471c9a407594dffe427bf
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2017-12-25 23:55:08 +0530

    Send And Recieve Request

---
+++ app/src/main/java/com/example/pulkit/chatapp1/ProfileActivity.java
@@ -0,0 +1,170 @@
+package com.example.pulkit.chatapp1;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    //Firebase
+    private FirebaseFirestore mUserDatabase, mFriendReqDatabse;
+    private FirebaseUser mCurrentUser;
+
+    private ImageView mProfileImage;
+    private TextView mProfileStatus, mProfileName, mProfileFriendsCount;
+    private Button mProfileReqBtn;
+
+    private String mCurrent_State;
+
+    private String current_uid;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        final String user_id = getIntent().getStringExtra("user_id");
+        current_uid = mCurrentUser.getUid();
+
+
+        mProfileImage = findViewById(R.id.profile_image);
+        mProfileName = findViewById(R.id.profile_displayname);
+        mProfileStatus = findViewById(R.id.profile_status);
+        mProfileFriendsCount = findViewById(R.id.profile_totalfriends);
+        mProfileReqBtn = findViewById(R.id.profile_sendrequest);
+
+        mCurrent_State = "not friends";
+
+        mUserDatabase = FirebaseFirestore.getInstance();
+        mFriendReqDatabse = FirebaseFirestore.getInstance();
+
+
+        mUserDatabase.collection("users").document(current_uid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String name = documentSnapshot.getString("name");
+                    String image = documentSnapshot.getString("image");
+                    String thumb_image = documentSnapshot.getString("thumb_image");
+                    String status = documentSnapshot.getString("status");
+
+                    mProfileName.setText(name);
+                    mProfileStatus.setText(status);
+
+                    if (!image.equals("default"))
+                        Picasso.with(ProfileActivity.this)
+                                .load(image)
+                                .placeholder(R.drawable.default_avatar)
+                                .into(mProfileImage);
+
+
+                    //----------FRIEND LIST/REQUEST --------------
+                    mFriendReqDatabse.collection("friend_requests").document(current_uid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+//                            if(documentSnapshot.exists())
+                        }
+                    });
+
+
+                }
+
+            }
+        });
+
+        mProfileReqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mProfileReqBtn.setEnabled(false);
+
+                //-----------------NOT FRIENDS STATE---------
+
+                if (mCurrent_State.equals("not friends")) {
+                    Map<String, Object> nesteddata = new HashMap<>();
+                    nesteddata.put("request_type", "sent");
+                    Map<String, Object> Data = new HashMap<>();
+                    Data.put(user_id, nesteddata);
+
+                    mFriendReqDatabse.collection("friend_requests").document(current_uid)
+                            .set(Data, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()) {
+                                Map<String, Object> nesteddata = new HashMap<>();
+                                nesteddata.put("request_type", "recieved");
+                                Map<String, Object> Data = new HashMap<>();
+                                Data.put(current_uid, nesteddata);
+                                mFriendReqDatabse.collection("friend_requests").document(user_id)
+                                        .set(Data, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+
+                                        mProfileReqBtn.setEnabled(true);
+                                        mCurrent_State = "req_sent";
+                                        mProfileReqBtn.setText(R.string.Cancelfriendreq);
+
+                                        Toast.makeText(ProfileActivity.this, "Request Sent Successfully", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(ProfileActivity.this, "Failed Sending Request", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+
+                //-----------------CANCEL REQUEST STATE---------
+                if (mCurrent_State.equals("req_sent")) {
+                    Map<String, Object> Data = new HashMap<>();
+                    Data.put(user_id, FieldValue.delete());
+
+                    mFriendReqDatabse.collection("friend_requests").document(current_uid)
+                            .update(Data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Map<String, Object> Data = new HashMap<>();
+                            Data.put(current_uid, FieldValue.delete());
+                            mFriendReqDatabse.collection("friend_requests").document(user_id)
+                                    .update(Data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    mProfileReqBtn.setEnabled(true);
+                                    mCurrent_State = "not_friends";
+                                    mProfileReqBtn.setText("Send Friend Request");
+                                }
+                            });
+                        }
+                    });
+
+
+                }
+            }
+
+
+        });
+    }
+}
--- app/src/main/java/com/example/pulkit/chatapp1/UsersActivity.java
+++ app/src/main/java/com/example/pulkit/chatapp1/UsersActivity.java
@@ -1,20 +1,23 @@
 package com.example.pulkit.chatapp1;
 
+
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.StorageReference;
+import com.google.firebase.firestore.Query;
 
 public class UsersActivity extends AppCompatActivity {
 
@@ -22,7 +25,12 @@ public class UsersActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
 
-    private FirebaseFirestore mUsersDatabase;
+    private static final CollectionReference sChatCollection =
+            FirebaseFirestore.getInstance().collection("users");
+    /**
+     * Get the last 50 chat messages ordered by timestamp .
+     */
+    private static final Query sChatQuery = sChatCollection.limit(50);
 
 
     @Override
@@ -31,19 +39,14 @@ public class UsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_users);
 
         mToolbar = findViewById(R.id.user_appbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("All Users");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        setSupportActionBar(mToolbar);
-
-//        mUsersDatabase = FirebaseFirestore.getInstance().collection("users");
-
 
 
         mRecyclerView = findViewById(R.id.users_list);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
 
 
     }
@@ -53,22 +56,67 @@ public class UsersActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-////        FirebaseRecyclerAdapter<Users,UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(Users.class,R.layout.user_layout,UsersViewHolder.class) {
-//            @Override
-//            public UsersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//                return null;
-//            }
-//
-//            @Override
-//            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
-//
-//            }
-//        };
-//
+        Log.i("Checking Status", "onStart: ");
+        attachRecyclerViewAdapter();
+
+    }
+
+    private void attachRecyclerViewAdapter() {
+        final RecyclerView.Adapter adapter = newAdapter();
+        Log.i("Checking Status", "onStart: ");
+
+        // Scroll to bottom on new messages
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                mRecyclerView.smoothScrollToPosition(adapter.getItemCount());
+            }
+        });
+
+        mRecyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        adapter.getItemCount();
+        Log.i("Checking Status", "attachRecyclerViewAdapter: " + adapter.getItemCount());
 
 
     }
-    public class UsersViewHolder extends RecyclerView.ViewHolder{
+
+    private RecyclerView.Adapter newAdapter() {
+
+//        Query query = FirebaseFirestore.getInstance().collection("users");
+        Log.i("Checking Status", "onStart: ");
+        FirestoreRecyclerOptions<Users> options = new FirestoreRecyclerOptions.Builder<Users>()
+                .setQuery(sChatQuery, Users.class)
+                .build();
+        return new FirestoreRecyclerAdapter<Users, UsersViewHolder>(options) {
+            @Override
+            public UsersViewHolder onCreateViewHolder(ViewGroup group, int i) {
+                Log.i("Checking Status", "onStart: ");
+                return new UsersViewHolder(LayoutInflater.from(group.getContext())
+                        .inflate(R.layout.user_layout, group, false));
+
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
+                Log.i("Checking Status", "onStart: ");
+
+                holder.setName(model.getName());
+
+            }
+        };
+
+
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
 
         View mView;
 
@@ -77,5 +125,15 @@ public class UsersActivity extends AppCompatActivity {
 
             mView = itemView;
         }
+
+        public void setName(String name) {
+
+            TextView userNameView = mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+            Log.i("Get Data", "onBindViewHolder: " + name);
+
+
+        }
     }
+
 }
